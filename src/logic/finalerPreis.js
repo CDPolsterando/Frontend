d2f3ff4e4e3276fe356ecab223ee430fa3184b5a
JohannesKaufmann <johannes@joina.de>
2017-07-28 17:13:09 +0200

    added logic to get minimum price and added tests

--- src/logic/finalerPreis.js
+++ src/logic/finalerPreis.js
@@ -1,31 +1,97 @@
-const stundenlohn_arbeit = 16
-const stundenlohn_transport = 10
+const getMindestpreis = (
+  {
+    stundensatz_fahrt,
+    stundensatz_arbeit,
+    pauschal_nacharbeit,
+    pauschal_reinigunsmittel,
+    cost_per_order,
+    spritpreis_pro_km,
+    mindest_marge
+  },
+  { produkte, fahrzeit, fahrstrecke },
+  { arbeitszeit, marge }
+) => {
+  /*
+  ((Fahrzeit*StundensatzFahrt*1,31*2)+
+  (Fahrstrecke*TankpauschaleProKM*2)+
+  (Arbeitszeit*StundensatzArbeit*1,31)+
+  (MaterialPauschale)+(ReinigungsmittelPauschale)+
+  (CPOPauschale))/(1-Marge=5%)*1,19
+  */
+  const preis =
+    (fahrzeit / 60 * stundensatz_fahrt * 1.31 * 2 +
+      fahrstrecke * spritpreis_pro_km * 2 +
+      arbeitszeit / 60 * stundensatz_arbeit * 1.31 +
+      pauschal_nacharbeit +
+      pauschal_reinigunsmittel +
+      cost_per_order) /
+    (1 - mindest_marge) *
+    1.19
 
-const pauschal_nacharbeit = 5
-const pauschal_reinigungsmittel = 15
-
-const preis_pro_km = 0.16
-
-// const input = {
-//   produkte: [
-//     {zeit: 2}, // stunden
-//     {zeit: 1}
-//   ],
-//   anfahrts_zeit: 1, // stunde
-//   km: 60
-// }
-
-const getPrice = ({ produkte, anfahrts_zeit, km }) => {
-  const auftragszeit_gesamt = produkte.reduce((acc, val) => acc + val.zeit, 0)
-  console.log(auftragszeit_gesamt)
-
-  const price =
-    auftragszeit_gesamt * stundenlohn_arbeit +
-    anfahrts_zeit * stundenlohn_transport +
-    pauschal_nacharbeit +
-    pauschal_reinigungsmittel +
-    km * preis_pro_km
-
-  return price
+  return preis
 }
-export default getPrice
+
+const getMarge = (
+  {
+    // konstanten
+    stundensatz_fahrt,
+    stundensatz_arbeit,
+    spritpreis_pro_km,
+    pauschal_nacharbeit,
+    pauschal_reinigunsmittel,
+    cost_per_order
+  },
+  {
+    // input
+    fahrzeit,
+    fahrstrecke
+  },
+  {
+    // bereits gerechnet
+    arbeitszeit,
+    bruttopreis
+  }
+) => {
+  /*
+  1-(((Fahrzeit*StundensatzFahrt*1,31*2)
+  +(Fahrstrecke*TankpauschaleProKM*2)
+  +(Arbeitszeit*StundensatzArbeit*1,31)
+  +(Nacharbeitspauschale)+(ReinigungsmittelPauschale)
+  +(CPOPauschale))/(BruttoPreis/1,19))
+  */
+
+  const marge =
+    1 -
+    (fahrzeit / 60 * stundensatz_fahrt * 1.31 * 2 +
+      fahrstrecke * spritpreis_pro_km * 2 +
+      arbeitszeit / 60 * stundensatz_arbeit * 1.31 +
+      pauschal_nacharbeit +
+      pauschal_reinigunsmittel +
+      cost_per_order) /
+      (bruttopreis / 1.19)
+
+  return marge * 100
+}
+const getInfo = (konstanten, input) => {
+  const arbeitszeit = input.produkte.reduce((acc, val) => {
+    const zeit = val.einzelzeit * (val.qm || 1)
+    return acc + zeit
+  }, 0)
+  const bruttopreis = input.produkte.reduce((acc, val) => {
+    const preis = val.einzelpreis_brutto * (val.qm || 1)
+    return acc + preis
+  }, 0)
+
+  const marge = getMarge(konstanten, input, { arbeitszeit, bruttopreis })
+
+  return {
+    marge,
+    mindestpreis: getMindestpreis(konstanten, input, {
+      arbeitszeit,
+      marge
+    })
+  }
+}
+
+// const margeZuPreis = ()
+export default getInfo
--- src/logic/finalerPreis.test.js
+++ src/logic/finalerPreis.test.js
@@ -1,16 +1,34 @@
 import finalerPreis from './finalerPreis'
 
+const konstanten = {
+  // alle einheiten in euro
+  stundensatz_fahrt: 10,
+  stundensatz_arbeit: 15,
+  pauschal_nacharbeit: 5,
+  pauschal_reinigunsmittel: 15,
+  cost_per_order: 45,
+  spritpreis_pro_km: 0.16,
+
+  mindest_marge: 0.05 // also 5%
+}
+
 describe('finalerPreis', () => {
-  it('can handle one couch', () => {
+  it('can handle "Mittlere Eckcouch"', () => {
     const input = {
       produkte: [
-        { zeit: 2 } // stunden
-        // {zeit: 1}
+        {
+          name: 'Mittlere Eckcouch',
+          einzelpreis_netto: 193.17, // euro
+          einzelpreis_brutto: 229.87, // euro
+          einzelzeit: 120 // minuten
+        }
       ],
-      anfahrts_zeit: 1, // stunde
-      km: 60
+      fahrzeit: 40.266666, // minuten
+      fahrstrecke: 49.135 // km
     }
-    const result = finalerPreis(input)
-    expect(result).toBe(229)
+    const result = finalerPreis(konstanten, input)
+    expect(result.mindestpreis).toBeCloseTo(172.37, 2)
+    expect(result.marge).toBeCloseTo(28.76, 2)
+    console.log(result)
   })
 })
